3646124e4a62aabcbc53be51ccadce996cf5b1f4
sonar-scape-designs <57213648+sonar-scape-designs@users.noreply.github.com>
2020-04-02 16:22:09 +0200

    esther - fetch js from t7

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function getCategories() {
+fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property")
+    .then(function (display) {
+        return display.json()
+    })
+    .then(displayCategories)
 
-    fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/categories?parent=41")
-        .then(res => res.json())
-        .then(setupCategories);
-}
+function displayCategories(data) {
+    //    console.log(data)
+    data.forEach(function (eachItem) {
+        console.log(eachItem)
+        const a = document.createElement("li");
+        //      a.textContent = eachItem.title.rendered;
+        document.querySelector("#myPosts").appendChild(a);
+        const section = document.createElement("section");
+        section.innerHTML = eachItem.title.rendered;
+        document.querySelector("body").appendChild(section);
 
-function setupCategories(catArray) {
-    const template = document.querySelector("template#categorytemplate").content;
-    const parentElement = document.querySelector("main");
-    catArray.forEach(cat => {
-        const copy = template.cloneNode(true);
     })
 
 }

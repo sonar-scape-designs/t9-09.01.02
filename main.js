2c2f8302882fcde816be294182afa2b35755f9a6
sonar-scape-designs <57213648+sonar-scape-designs@users.noreply.github.com>
2020-04-03 11:50:15 +0200

    button added. next step categories

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property/")
+
+
+
+fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property?_embed")
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         showProperties(data)
     })
+
 
 
 function showProperties(jsonData) {
@@ -15,7 +19,7 @@ function showProperties(jsonData) {
 function showEachProperty(property) {
     console.log(property)
 
-    const template = document.querySelector("#propeties-temp").content;
+    const template = document.querySelector("#properties-temp").content;
     const copy = template.cloneNode(true);
     copy.querySelector("h3").textContent = property.title.rendered;
     copy.querySelector(".rooms").textContent = property.bedroom;
@@ -24,8 +28,43 @@ function showEachProperty(property) {
     copy.querySelector(".properties-info").textContent = property.content.rendered;
 
     document.querySelector("#popular").appendChild(copy)
-
-
-
-
 }
+
+//add images
+
+//const imageName = property.source_url;
+//    const base = "http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property?_embed";
+//    const smallImg = base + "small/" + imageName + "-sm.jpg";
+//    const mediumImg = base + "medium/" + imageName + "-md.jpg";
+//    const largeImg = base + "large/" + imageName + ".jpg";
+//    clone.querySelector("img").src = smallImg;
+
+
+//fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property?_embed")
+//    .then(function (response) {
+//        return response.json()
+//    })
+//    .then(function (data) {
+//        showImages(data)
+//    })
+//
+//
+//function showImages(jsonData) {
+//    //        console.log(jsonData)
+//    jsonData.forEach(showEachProperty)
+//}
+//
+//function showEachImage(image) {
+//    console.log(property)
+//
+////    const template = document.querySelector("#properties-temp").content;
+////    const copy = template.cloneNode(true);
+////
+////    copy.querySelector("img").src = image.source_url;
+////
+//    var img = document.createElement('img');
+//    img.src = images[i].source_url;
+//
+//    document.querySelector(".info").appendChild(copy)
+//    console.log(image)
+//}

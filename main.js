1da6d65e29b695b9d0e52851aecacd28d39ca535
sonar-scape-designs <57213648+sonar-scape-designs@users.noreply.github.com>
2020-04-03 10:25:36 +0200

    working fect with data (no images)

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
-fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property")
-    .then(function (display) {
-        return display.json()
+fetch("http://sonarscapedesigns.com/2sem/T9/wp-json/wp/v2/property/")
+    .then(function (response) {
+        return response.json()
     })
-    .then(displayCategories)
-
-function displayCategories(data) {
-    //    console.log(data)
-    data.forEach(function (eachItem) {
-        console.log(eachItem)
-        const a = document.createElement("li");
-        //      a.textContent = eachItem.title.rendered;
-        document.querySelector("#myPosts").appendChild(a);
-        const section = document.createElement("section");
-        section.innerHTML = eachItem.title.rendered;
-        document.querySelector("body").appendChild(section);
-
+    .then(function (data) {
+        showProperties(data)
     })
 
+
+function showProperties(jsonData) {
+//        console.log(jsonData)
+    jsonData.forEach(showEachProperty)
+}
+
+function showEachProperty(property) {
+    console.log(property)
+
+    const template = document.querySelector("#propeties-temp").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("h3").textContent = property.title.rendered;
+    copy.querySelector(".rooms").textContent = property.bedroom;
+    copy.querySelector(".bath").textContent = property.bathroom;
+    copy.querySelector(".size").textContent = property.feet;
+
+    document.querySelector("#popular").appendChild(copy)
 }
